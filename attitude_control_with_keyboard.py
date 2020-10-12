f7a810a0158a4595f653e182934cdf5f630ce8c3
shubham9630 <66088224+shubham9630@users.noreply.github.com>
2020-10-12 11:21:20 -0400

    Update attitude_control_with_keyboard.py

--- attitude_control_with_keyboard.py
+++ attitude_control_with_keyboard.py
@@ -52,7 +52,7 @@ def arm_and_takeoff(altitude):
  #     time.sleep(1)
 
    print("Arming motors")
-   vehicle.mode = VehicleMode("GUIDED_NOGPS")
+   vehicle.mode = VehicleMode("GUIDED")
    vehicle.armed = True
 
    #while not vehicle.armed:
